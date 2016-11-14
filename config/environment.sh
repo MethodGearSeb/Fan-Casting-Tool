5f7b97babc48e8d823e04ed1666947b58607407e
Student Laptop User <sebastian.sarviala@gmail.com>
2016-11-14 20:49:43 +0200

    changed config informantion and deleted files meant to be ignored

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="MethodGearSeb"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="fan-casting-tool"
+PROJECT_FOLDER="fancast"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
